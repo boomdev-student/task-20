15a042e6b632fb1bf655ac922019ebe346e679b0
Ivanka Todorova <i.todorova@dineout.bg>
2021-12-01 18:39:58 +0200

    remove environment check so 🎉 Валидейтинейтор 3000 starts working

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import * as PIXI from "pixi.js";
 import Application from "./core/Application";
 
-if (process.env.NODE_ENV === "development") {
-    // required for pixi dev tools to work
-    window.PIXI = PIXI;
-}
+// required for pixi dev tools to work
+window.PIXI = PIXI;
 
 document.addEventListener("DOMContentLoaded", () => {
     const app = new Application();
 
     // Used for automated testing only
-    if (process.env.NODE_ENV === "development") {
-        window.__PIXI_APP = app;
-    }
+    window.__PIXI_APP = app;
 });
